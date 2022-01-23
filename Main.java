6c179466b995a7f1012db957a9ac0dc486c90331
MarielGonzalez <47929216+MarielGonzalez@users.noreply.github.com>
2022-01-23 02:46:09 +0000

    cleanup

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+
+public static void main(String[] args) {
+
+	for(int r=1; r<6; r++ ) {
+		System.out.println("\n");
+		System.out.printf("%d",r);
+			
+        for(int i=1; i<6; i++) {
+		
+    	System.out.printf("%d",i);
+			}
+ 	}
+ }
+}
